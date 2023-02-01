9894a8f112956006ea3a58afd419a64b42e0cb42
eduardferre <eduard.ferre.sanchez@estudiantat.upc.edu>
2023-02-01 19:27:33 +0100

    backend2

--- backend/fastAPI/users.py
+++ backend/fastAPI/users.py
@@ -30,14 +30,40 @@ async def userById(id: int):
 @app.post("/user/")
 async def user(user: User):
     if type(search_user(user.id)) == User:
-        return { "error": f"The user {user.name} already exists"}
-    else:
-        users_list.append(user)
-        return "200 OK"
-'''
+        return { "error": f"The user {user.id} already exists"}
+    
+    users_list.append(user)
+    return user
+
 @app.put("/user/")
 async def user(user: User):
-'''
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+            print(found)
+    
+    if not found:
+        return { "error": f"The user {user.name} has not been updated"  }
+    
+    return user
+
+@app.delete("/user/{id}")
+async def user(id: int):
+
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+            del users_list[index]
+            found = True
+    
+    if not found:
+        return { "error": f"The user {user.name} has not been deleted"  }
+    
 
 
 
