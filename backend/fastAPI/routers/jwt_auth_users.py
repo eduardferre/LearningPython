8fc6960b888ce7be2729f32269f3458a00bf6dd7
eduardferre <eduard.ferre.sanchez@estudiantat.upc.edu>
2023-02-06 18:10:07 +0100

    jwt

---
+++ backend/fastAPI/routers/jwt_auth_users.py
@@ -0,0 +1,104 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import jwt, JWTError
+from passlib.context import CryptContext
+from datetime import datetime, timedelta
+
+ALGORITHM = "HS256"
+ACCESS_TOKEN_DURATION = 1 # 1 min
+SECRET = "7b00fffc87f610405923b507b13bda8356dc34dce447cf4dfe4bfa7b7dece23f" # openssl rand -hex 32
+
+app = FastAPI()
+
+oauth2 = OAuth2PasswordBearer(tokenUrl="login")
+
+crypt = CryptContext(schemes=["bcrypt"])
+
+class User(BaseModel):
+    username: str
+    full_name: str
+    email: str
+    disabled: bool
+
+
+class UserDB(User):
+    password: str
+
+
+users_db = {
+    "mouredev": {
+        "username": "mouredev",
+        "full_name": "Brais Moure",
+        "email": "braismoure@mourede.com",
+        "disabled": False,
+        "password": "$2a$12$vCFqRGZiUhdXZbI4DdJEAe.oC.d/KrEWYYPeuggPfqL0/5x.ZWw7q"
+    },
+    "mouredev2": {
+        "username": "mouredev2",
+        "full_name": "Brais Moure 2",
+        "email": "braismoure2@mourede.com",
+        "disabled": True,
+        "password": "$2a$12$LnJTMQk7Hu3Lp/RiUMkPAOZs/NL7RPOVV75eZLt9CGoMeSd6Q8PTG"
+    }
+}
+
+def search_user_db(username: str):
+    if username in users_db:
+        return UserDB(**users_db[username]) # Solo se usa para la autenticación
+
+def search_user(username: str):
+    if username in users_db:
+        return User(**users_db[username]) # De esta forma no se pasa el usuario con la contraseña
+
+async def auth_user(token: str = Depends(oauth2)):
+
+    exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Credenciales de autenticación inválidas",
+            headers={"WWW-Authenticate": "Bearer"})
+
+    try:
+        username = jwt.decode(token, SECRET, algorithms=[ALGORITHM]).get("sub")
+        if username is None:
+            raise exception
+
+    except JWTError:
+        raise exception
+
+    return search_user(username)
+        
+
+async def current_user(user: User = Depends(auth_user)):
+    if user.disabled:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Usuario inactivo")
+
+    return user
+
+@app.post("/login")
+async def login(form: OAuth2PasswordRequestForm = Depends()):
+    user_db = users_db.get(form.username)
+    if not user_db:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="El usuario no es correcto")
+
+    user = search_user_db(form.username)
+
+    if not crypt.verify(form.password, user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail="La contraseña no es correcta")
+
+    expire = datetime.utcnow() + timedelta(minutes=ACCESS_TOKEN_DURATION)
+
+    access_token = {
+        "sub": user.username,
+        "exp": expire,
+    }
+
+    return {"access_token": jwt.encode(access_token, SECRET, algorithm=ALGORITHM), "token_type": "bearer"}
+
+@app.get("/users/me")
+async def me(user: User = Depends(current_user)):
+    return user
